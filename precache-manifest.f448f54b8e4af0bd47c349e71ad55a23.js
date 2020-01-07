3104fe5ad56abea27ae0d12ad8ce9a3174f9cd9b
Mitchell Kimm <kim3603di@gmail.com>
2020-01-07 14:13:02 +0900

    Updates

---
+++ precache-manifest.f448f54b8e4af0bd47c349e71ad55a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fcc5870d4500582475494a3b50e56e",
+    "url": "/polic_admin_react/index.html"
+  },
+  {
+    "revision": "c25c22b826b7f4722142",
+    "url": "/polic_admin_react/static/css/main.1536519c.chunk.css"
+  },
+  {
+    "revision": "da6077ed28409ed5854f",
+    "url": "/polic_admin_react/static/js/2.f436b61d.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/polic_admin_react/static/js/2.f436b61d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c25c22b826b7f4722142",
+    "url": "/polic_admin_react/static/js/main.4a0b250b.chunk.js"
+  },
+  {
+    "revision": "80486f6dc974ada32af5",
+    "url": "/polic_admin_react/static/js/runtime-main.c543cf01.js"
+  }
+]);
